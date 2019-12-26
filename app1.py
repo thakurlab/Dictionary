9cc0d7ad407ae2122c1b7064eadd9b58d8717628
thakurlab <rohitxthakur20@gmail.com>
2019-12-26 18:16:56 +0530

    3

--- app1.py
+++ app1.py
@@ -7,6 +7,10 @@ def meaning(w):
 	w = w.lower()
 	if w in data:
 		return data[w]
+	elif w.title() in data:
+		return data[w.title()]
+	elif w.upper() in data:
+		return data[w.upper()]
 	elif len(get_close_matches(w,data.keys())) > 0:
 		yn = input("Did you mean %s ? Enter 1 if yes, or 0 if no : " % get_close_matches(w, data.keys())[0])
 		if yn == 1:
